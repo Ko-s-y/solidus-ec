6728e69ad966926c28e0f417fcaddfb1137cd2f6
Ko-s-y <song.1209@icloud.com>
2022-05-28 16:25:30 +0900

    application_helper.rbのrubocopによる余分な改行及びspace忘れの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
-
   BASE_TITLE = "BIGBAG Store".freeze
 
   def full_title(page_title)
-    if page_title.blank?  #toppageなどpage_titleが空,nilの対策
+    if page_title.blank?  # toppageなどpage_titleが空,nilの対策
       BASE_TITLE
     else
       "#{page_title.upcase} - #{BASE_TITLE}"
     end
   end
-
 end
