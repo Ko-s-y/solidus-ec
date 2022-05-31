0ff63fc5bd4b90dd37ee97f961b0e502141c5b5f
Ko-s-y <song.1209@icloud.com>
2022-05-31 21:44:08 +0900

    蛇足となるコメント、不要な空行の削除及びjsの読み込み位置の改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   BASE_TITLE = "BIGBAG Store".freeze
 
   def full_title(page_title)
-    if page_title.blank?  # toppageなどpage_titleが空,nilの対策
+    if page_title.blank?
       BASE_TITLE
     else
       "#{page_title} - #{BASE_TITLE}"
