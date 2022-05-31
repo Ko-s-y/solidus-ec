2a706cb4545bc867d40c156df845cd9ae6a17a60
Ko-s-y <song.1209@icloud.com>
2022-06-01 00:26:45 +0900

    商品情報の確認をするtestの値を追加

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "#show" do
-    let(:product) { create(:product) }
+    let(:product) { create(:product, name: "sample_product", price: "10.0", description: "This is sample!") }
 
     it "returns 200 response & success" do
       get potepan_product_path(product.id)
@@ -10,10 +10,11 @@ RSpec.describe "Potepan::Products", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    let(:product) { create(:product, name: "sample_product") }
-    it "check product name" do
+    it "correct product value" do
       expect(product.name).to eq "sample_product"
+      expect(product.slug).to eq "sample_product"
+      expect(product.price.to_s).to eq "10.0"
+      expect(product.description).to eq "This is sample!"
     end
-
   end
 end
