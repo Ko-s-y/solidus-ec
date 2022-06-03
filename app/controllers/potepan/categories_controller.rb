6d3c064c23984c2e3848b16911dcba90d46ac3ed
Ko-s-y <song.1209@icloud.com>
2022-06-03 17:54:51 +0900

    all_productsメソッドの挙動確認

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
+    @products = @taxon.all_products
+    @product = @products.first
   end
 end
