4e70a2a41e7ca980604c9b46bcddbf68bc8fbb0f
Ko-s-y <song.1209@icloud.com>
2022-06-04 20:57:28 +0900

    products_specの更新

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "#show" do
-    let(:product) { create(:product) }
+    let(:taxon) { create(:taxon) }
+    let(:product) { create(:product, taxons: [taxon]) }
 
     before do
       get potepan_product_url(product.id)
