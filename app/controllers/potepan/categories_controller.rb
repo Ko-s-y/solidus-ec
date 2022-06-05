aef610356d7fb93187a97323e3695bd5b9ab8043
Ko-s-y <song.1209@icloud.com>
2022-06-05 12:32:12 +0900

    categories_controllerにincludesを適用

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products
-    @taxonomies = Spree::Taxonomy.all
+    @products = @taxon.all_products.includes(master: [:images, :default_price])
+    @taxonomies = Spree::Taxonomy.includes(:root)
   end
 end
