3cca73d0be0439759366d6299c8e15d2ce6e263e
Ko-s-y <song.1209@icloud.com>
2022-06-06 21:04:53 +0900

    productsのfeutureのテスト追加

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Potepan::Products_feature", type: :feature do
   feature "#show" do
-    given!(:taxon) { create(:taxon) }
-    given!(:taxonomy) { create(:taxonomy) }
-    given!(:product) { create(:product, taxons: [taxon]) }
+    given(:taxon) { create(:taxon) }
+    given(:taxonomy) { create(:taxonomy) }
+    given(:product) { create(:product, taxons: [taxon]) }
 
     background do
       visit potepan_product_path(product.id)
@@ -12,6 +12,29 @@ RSpec.feature "Potepan::Products_feature", type: :feature do
 
     scenario "redirect to /potepan" do
       expect(page).to have_link "Home", href: potepan_path
+    end
+
+    scenario "redirect to /potepan/categories/:id" do
+      expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
+    end
+
+    scenario "displayed and correct page title" do
+      expect(page).to have_title "#{product.name} - BIGBAG Store"
+    end
+
+    scenario "correct product path" do
+      expect(page).to have_current_path potepan_product_path(product.id)
+    end
+
+    scenario "displayed and correct product value in products/show" do
+      expect(page).to have_selector ".media-body h2", text: product.name
+      expect(page).to have_selector ".media-body h3", text: product.display_price
+      expect(page).to have_selector ".media-body p", text: product.description
+    end
+
+    scenario "displayed and correct product value in shared/light_section" do
+      expect(page).to have_selector ".col-xs-6 h2", text: product.name
+      expect(page).to have_selector ".active", text: product.name
     end
   end
 end
