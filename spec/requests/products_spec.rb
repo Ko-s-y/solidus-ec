8c2cbb1427ea1ce2a9586e464cd5f896791da4be
Ko-s-y <song.1209@icloud.com>
2022-06-07 15:49:25 +0900

    カテゴリー名テスト実装

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe "Potepan::Products_request", type: :request do
     before do
       get potepan_product_url(product.id)
       product.images << image
-      # 画像URL取得が上手くいかない問題への対応
-      # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
       ActiveStorage::Current.host = request.base_url
     end
 
@@ -24,10 +22,7 @@ RSpec.describe "Potepan::Products_request", type: :request do
       expect(response.body).to include product.description
     end
 
-    it "get product image url" do
-
-      binding.pry
-
+    it "get product image info" do
       product.images { |image| expect(response.body).to include image.attachment(:prduct) }
     end
   end
