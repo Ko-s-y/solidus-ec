c33b54cbbcdbda4fd29df5752935a84a5c618272
Ko-s-y <song.1209@icloud.com>
2022-06-09 22:11:17 +0900

    products/showページに商品画像を仮表示

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @related_products = Spree::Product.all.includes(master: [:images, :default_price]).limit(4)
   end
 end
