78f4ef8d8159568850014383dc39ceed11d1153e
Ko-s-y <song.1209@icloud.com>
2022-06-10 19:30:21 +0900

    related_productsメソッドに引数追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.limit(4)
+    @related_products = @product.related_products(3)
   end
 end
--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,11 +1,12 @@
 module Potepan::ProductDecorator
-  def related_products
+  def related_products(display_limit)
     Spree::Product.
       all.
       includes(master: [:images, :default_price]).
       in_taxons(taxons).
       where.not(id: id).
-      distinct
+      distinct.
+      limit(display_limit)
   end
   Spree::Product.prepend self
 end
