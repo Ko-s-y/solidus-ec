cbf0f6bb17cb2d640335fbfb54ea41f06b222bff
Ko-s-y <song.1209@icloud.com>
2022-06-12 00:05:50 +0900

    modelテストのサンプルデータ追加, 検証

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe "Product model of Potepanec", type: :model do
   describe "#related_products" do
     let(:taxon) { create(:taxon) }
     let(:other_taxon) { create(:taxon) }
-    let(:product) { create(:product, taxons: [taxon, other_taxon]) }
-    let(:other_product) { create(:product, taxons: [taxon, other_taxon]) }
+    let(:product) { create(:product, taxons: [taxon]) }
+    let(:other_product) { create(:product, taxons: [other_taxon]) }
+    let(:related_products) { create_list :product, 4, taxons: [taxon] }
+    let(:duplicate_related_product) { create :product, taxons: [taxon] }
+    let(:not_related_product) { create :product, taxons: [other_taxon] }
 
     it "product is not belong to related products" do
       expect(product.related_products).not_to include product
@@ -13,6 +16,7 @@ RSpec.describe "Product model of Potepanec", type: :model do
 
     it "related products are not duplicate" do
       expect(product.related_products).to eq product.related_products.uniq
+      expect(product.related_products).not_to eq duplicate_related_product
     end
 
     it "order by product id asc" do
