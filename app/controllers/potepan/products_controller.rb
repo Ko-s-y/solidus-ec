9c72f3fedcf14014715bdde1e516e4b0f9befa5b
Ko-s-y <song.1209@icloud.com>
2022-06-11 16:10:33 +0900

    includesをモデルに切り出してしまっていた為コントローラーに切り戻し

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,6 @@ class Potepan::ProductsController < ApplicationController
   DISPLAYED_PRODUCTS_LIMIT = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.limit(DISPLAYED_PRODUCTS_LIMIT)
+    @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(DISPLAYED_PRODUCTS_LIMIT)
   end
 end
--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -2,7 +2,6 @@ module Potepan::ProductDecorator
   def related_products
     Spree::Product.
       all.
-      includes(master: [:images, :default_price]).
       in_taxons(taxons).
       where.not(id: id).
       distinct
