6c651c91f6e70ef652c6025e0b9d4bb195178d6f
Ko-s-y <song.1209@icloud.com>
2022-06-14 14:31:16 +0900

    モデルテスト改修, テストデータ作成

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,30 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Product model of Potepanec", type: :model do
-  describe "#related_products" do
-    let(:taxon) { create(:taxon) }
+RSpec.describe Potepan::ProductDecorator, type: :model do
+  describe "related_products" do
+    let(:taxon1) { create(:taxon) }
+    let(:taxon2) { create(:taxon) }
+    let(:taxon3) { create(:taxon) }
+    let(:taxon4) { create(:taxon) }
     let(:other_taxon) { create(:taxon) }
-    let(:product) { create(:product, taxons: [taxon]) }
-    let(:other_product) { create(:product, taxons: [other_taxon]) }
-    let(:related_products) { create_list :product, 4, taxons: [taxon] }
-    let(:duplicate_related_product) { create :product, taxons: [taxon] }
+    let!(:main_product) { create(:product, taxons: [taxon1, taxon2, taxon3, taxon4]) }
+    let!(:related_product1) { create(:product, taxons: [taxon1, taxon2]) }
+    let!(:related_product2) { create(:product, taxons: [taxon2, taxon3]) }
+    let!(:related_product3) { create(:product, taxons: [taxon3, taxon4]) }
+    let!(:related_product4) { create(:product, taxons: [taxon4, taxon1]) }
     let!(:not_related_product) { create :product, taxons: [other_taxon] }
-
-    it "product is not belong to related products" do
-      expect(product.related_products).not_to include product
-    end
-
-    it "not related product is not include" do
-      expect(product.related_products).not_to include not_related_product
-    end
-
-    it "related products are not duplicate" do
-      expect(product.related_products).to eq product.related_products
-      expect(product.related_products).not_to eq duplicate_related_product
-    end
-
-    it "order by product id asc" do
-      expect(product.related_products).to match related_products
-    end
-  end
 end
