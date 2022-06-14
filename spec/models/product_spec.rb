de4d760c734de71878c2bcb8b93311489e2cae09
Ko-s-y <song.1209@icloud.com>
2022-06-15 01:26:14 +0900

    ralated_productのorderテスト追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
     let(:taxon1) { create(:taxon) }
     let(:taxon2) { create(:taxon) }
     let(:taxon3) { create(:taxon) }
-    let(:taxon4) { create(:taxon) }
     let(:other_taxon) { create(:taxon) }
-    let(:main_product) { create(:product, taxons: [taxon1, taxon2, taxon3, taxon4]) }
-    let!(:related_product1) { create(:product, taxons: [taxon1, taxon3]) }
-    let!(:related_product2) { create(:product, taxons: [taxon2, taxon4]) }
+    let(:main_product) { create(:product, taxons: [taxon1, taxon2, taxon3]) }
+    let!(:related_product1) { create(:product, taxons: [taxon1]) }
+    let!(:related_product2) { create(:product, taxons: [taxon2]) }
+    let!(:related_product3) { create(:product, taxons: [taxon3]) }
     let!(:not_related_product) { create(:product, taxons: [other_taxon]) }
 
     it "main product not to include related products" do
@@ -19,6 +19,7 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
     it "related product to include related products" do
       expect(main_product.related_products).to include related_product1
       expect(main_product.related_products).to include related_product2
+      expect(main_product.related_products).to include related_product3
     end
 
     it "not related product not to include in related products" do
@@ -27,6 +28,10 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
 
     it "related products are not duplicate" do
       expect(main_product.related_products == main_product.related_products.uniq).to be true
+    end
+
+    it "related products are correctly sort order" do
+      expect(main_product.related_products).to eq [related_product1, related_product2, related_product3]
     end
   end
 end
